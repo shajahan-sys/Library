c7cca50f4991e1eab12b99fc0b5074ca3a2e904b
BG678 <57709951+BG678@users.noreply.github.com>
2020-07-20 14:49:21 +0200

    added packages and main class

---
+++ library/src/main/java/org/library/application/Application.java
@@ -0,0 +1,7 @@
+package org.library.application;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ library/src/main/java/org/library/hibernate_util/HibernateUtil.java
@@ -0,0 +1,4 @@
+package org.library.hibernate_util;
+
+public class HibernateUtil {
+}
