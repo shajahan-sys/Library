6a88f69702ca58e5b6ff05af2a897023f604401f
BG678 <57709951+BG678@users.noreply.github.com>
2020-09-26 22:28:03 +0200

    Added validation constraints to Book entity

--- library/src/main/java/org/library/features/book/Book.java
+++ library/src/main/java/org/library/features/book/Book.java
@@ -5,24 +5,37 @@ import org.library.features.login.Login;
 import org.library.features.lend_book.Lending;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
 
+/**
+ * POJO class that represents entity Book
+ *
+ * @author Barbara Grabowska
+ * @version %I%
+ */
 @Entity
 @Table(name = "books")
-public class Book {
+public class Book implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotNull
+    @Size(max = 45)
     @Column(name = "title")
     private String title;
+    @NotNull
+    @Size(max = 4)
     @Column(name = "publication_year")
     private String publicationYear;
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "author_id", nullable = false)
     private Author author;
     @OneToOne(mappedBy = "book")
     private Lending lending;
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private Login login;
@@ -32,7 +45,6 @@ public class Book {
 
     public Book(int id) {
         this.id = id;
-        List l = new ArrayList();
     }
 
     public int getId() {
@@ -50,8 +62,7 @@ public class Book {
     public void setTitle(String title) {
         this.title = title;
     }
-
-
+    
     public String getPublicationYear() {
         return publicationYear;
     }
@@ -59,7 +70,6 @@ public class Book {
     public void setPublicationYear(String publicationYear) {
         this.publicationYear = publicationYear;
     }
-
 
     public Author getAuthor() {
         return author;
