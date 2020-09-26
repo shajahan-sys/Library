e89b7a53e233da0ab0acabd687783d8de3107a0a
BG678 <57709951+BG678@users.noreply.github.com>
2020-09-26 21:43:47 +0200

    Added validation constraints to Author entity

--- library/src/main/java/org/library/features/author/Author.java
+++ library/src/main/java/org/library/features/author/Author.java
@@ -4,18 +4,30 @@ import org.library.features.book.Book;
 import org.library.features.login.Login;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
 import java.util.Set;
 
+/**
+ * POJO class that represents entity Author
+ *
+ * @author Barbara Grabowska
+ * @version %I%
+ */
 @Entity
 @Table(name = "authors")
-public class Author {
+public class Author implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotNull
+    @Size(max = 45)
     @Column(name = "surname")
     private String surname;
     @OneToMany(mappedBy = "author", fetch = FetchType.EAGER)
     private Set<Book> books;
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private Login login;
