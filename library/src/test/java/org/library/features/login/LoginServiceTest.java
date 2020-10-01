538d2e50696aec6ed25c6da45a6d8f9011abdeef
BG678 <57709951+BG678@users.noreply.github.com>
2020-10-01 18:20:17 +0200

    Changed LoginService tests according to changes in Login Service class

--- library/src/test/java/org/library/features/login/LoginServiceTest.java
+++ library/src/test/java/org/library/features/login/LoginServiceTest.java
@@ -1,86 +1,90 @@
 package org.library.features.login;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.library.hibernate_util.HibernateTestUtil;
-
-import javax.persistence.Query;
+import org.mindrot.jbcrypt.BCrypt;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class LoginServiceTest {
     private Login login;
-    private LoginService service;
-    private SessionFactory sessionFactory;
+    private LoginService loginService;
+    private LoginDao loginDao;
+    @Mock
+    private LoginDAOImpl loginDaoImpl;
 
     @BeforeEach
     void setUp() {
-        sessionFactory = HibernateTestUtil.getTestSessionFactory();
-        service = new LoginService();
-        service.setSessionFactory(sessionFactory);
-        createLogin();
+        MockitoAnnotations.openMocks(this);
+        login = new Login();
+        login.setPassword("pass");
+        loginDao = loginDaoImpl;
+        loginService = new LoginService();
+        loginService.setLoginDao(loginDao);
     }
 
     @AfterEach
     void tearDown() {
-        deleteLoginFormTestDB(login);
-        sessionFactory.close();
-        service = null;
+        loginService = null;
+        loginDao = null;
         login = null;
     }
 
     @Test
-    void shouldAllowCreatingNewAccountWithTheSameLoginJustOnce() {
-        String expected = "User already exist, try different login or login instead of creating new account";
+    void test_isLoginInputCorrect_when_password_is_correct() {
+        String hashed = BCrypt.hashpw("pass", BCrypt.gensalt());
+        when(loginDao.getHashedPassword(login)).thenReturn(hashed);
         assertAll(
-                () -> assertTrue(service.createNewAccount(login)),
-                () -> assertFalse(service.createNewAccount(login)),
-                () -> assertEquals(expected, service.getMessage())
+                () -> assertTrue(loginService.isLoginInputCorrect(login)),
+                () -> assertEquals(login.getPassword(), "not storing real pass")
         );
-    }
-    @Test
-    void shouldReturnProperMessageAndFalseIfUserDoesntExist(){
-        String expected = "There is no such a user";
-        assertAll(
-                () -> assertFalse(service.loginToProperAccount(login)),
-                () -> assertEquals(expected, service.getMessage())
-        );
-    }
-    @Test
-    void shouldAllowUserToLoginToAccountThatAlreadyExistsIfProvidedPasswordIsCorrect() {
-        service.createNewAccount(login);
-        createLogin();
-        assertTrue(service.loginToProperAccount(login));
+
     }
 
     @Test
-    void shouldntAllowUserToLoginWithWrongPassword() {
-        String expected = "Wrong password!";
-        service.createNewAccount(login);
-        createLogin();
-        login.setPassword("wrong password");
+    void test_isLoginInputCorrect_when_password_is_not_correct() {
+        String wrongHashedPass = BCrypt.hashpw("wrong", BCrypt.gensalt());
+        when(loginDao.getHashedPassword(login)).thenReturn(wrongHashedPass);
         assertAll(
-                () -> assertFalse(service.loginToProperAccount(login)),
-                () -> assertEquals(expected, service.getMessage())
+                () -> assertFalse(loginService.isLoginInputCorrect(login)),
+                () -> assertEquals("Wrong password!", loginService.getMessage())
         );
     }
 
-    private void createLogin() {
-        login = new Login();
-        login.setUserName("test");
-        login.setPassword("correct password");
+    @Test
+    void test_createNewAccount() {
+        loginService.createNewAccount(login);
+        assertAll(
+                () -> verify(loginDao).saveNewAccount(login),
+                () -> assertEquals(login.getPassword(), "not storing real pass")
+        );
     }
 
-    private void deleteLoginFormTestDB(Login login) {
-        Session session = sessionFactory.openSession();
-        session.getTransaction().begin();
-        Query delete = session.createQuery("delete from Login where user_name = :username")
-                .setParameter("username", login.getUserName());
-        delete.executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+    @Test
+    void test_loginIfPossible_when_action_equals_login_and_user_does_not_exist() {
+        when(loginDao.doesUserAlreadyExist(login)).thenReturn(false);
+        assertAll(
+                () -> assertFalse(loginService.loginIfPossible(login, "login")),
+                () -> assertEquals(loginService.getMessage(), "There is no user with such a name")
+        );
+    }
+
+    @Test
+    void test_loginIfPossible_when_action_equals_create_and_user_exists() {
+        when(loginDao.doesUserAlreadyExist(login)).thenReturn(true);
+        assertAll(
+                () -> assertFalse(loginService.loginIfPossible(login, "create")),
+                () -> assertEquals(loginService.getMessage(), "User already exists, try different name")
+        );
+    }
+
+    @Test
+    void test_loginIfPossible_when_action_name_is_wrong() {
+        assertThrows(IllegalArgumentException.class, () -> loginService.loginIfPossible(login, "wrong name"));
     }
 }
