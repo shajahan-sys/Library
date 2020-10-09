1abcd572a0b8a7fe52ea4781f95aa2553b8c9cdd
BG678 <57709951+BG678@users.noreply.github.com>
2020-10-09 20:03:27 +0200

    Modified tests according to changes in ReaderController class

--- library/src/test/java/org/library/features/reader/ReaderControllerTest.java
+++ library/src/test/java/org/library/features/reader/ReaderControllerTest.java
@@ -25,12 +25,16 @@ import static org.mockito.Mockito.*;
 class ReaderControllerTest {
     private ReaderController readerController;
     private Reader reader;
+    private Login login;
+    private List<Reader> readers;
     @Mock
-    private ReaderService service;
+    private ReaderService readerService;
     @Mock
     HttpServletResponse resp;
     @Mock
     HttpServletRequest req;
+    @Mock
+    RequestDispatcher rd;
     @Spy
     HttpSession session;
     private final ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
@@ -39,41 +43,54 @@ class ReaderControllerTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         readerController = new ReaderController();
+        login = new Login();
+        when(req.getSession()).thenReturn(session);
+        when(session.getAttribute("userLogin")).thenReturn(login);
+        readerController.setReaderService(readerService);
     }
 
     @AfterEach
     void tearDown() {
         readerController = null;
+        login = null;
     }
 
     @Test
-    void test_setProperAttributesForwardRequest_when_session_attribute_readers_is_null() throws ServletException, IOException {
-        readerController.setReaderService(service);
-        readerController.setSession(session);
-        RequestDispatcher rd = mock(RequestDispatcher.class);
-        when(req.getRequestDispatcher(captor.capture())).thenReturn(rd);
-        List<Reader> list = new ArrayList<>();
-        when(service.getReadersList(new Login())).thenReturn(list);
-        when(session.getAttribute("readers")).thenReturn(null);
-        readerController.setProperAttributesForwardRequest(req, resp);
-        assertAll(() -> verify(session).setAttribute("readers", list),
-                () -> verify(req).getRequestDispatcher(captor.capture()),
-                () -> assertEquals("reader.jsp", captor.getValue()));
+    void test_setProperAttributes_when_save_attribute_is_null() {
+        readers = new ArrayList<>();
+        readers.add(new Reader());
+        when(readerService.getReadersList(login)).thenReturn(readers);
+        readerController.setProperAttributes(req);
+        assertAll(
+                () -> verify(readerService, Mockito.never()).deleteFromMap(login),
+                () -> verify(session).setAttribute("readers", readers)
+        );
     }
 
     @Test
-    void test_resolveReturn_when_selectedReaders_lendingSet_is_not_empty() throws IOException {
-        readerController.setReaderService(service);
-        readerController.setSession(session);
+    void test_setProperAttributes_when_save_attribute_is_true() {
+        readers = new ArrayList<>();
+        readers.add(new Reader());
+        when(readerService.getReadersList(login)).thenReturn(readers);
+        when(session.getAttribute("saved")).thenReturn(true);
+        readerController.setProperAttributes(req);
+        assertAll(
+                () -> verify(readerService).deleteFromMap(login),
+                () -> verify(session).removeAttribute("saved"),
+                () -> verify(session).setAttribute("readers", readers)
+        );
+    }
+
+    @Test
+    void test_returnAction_when_selectedReaders_lendingSet_is_not_empty() throws IOException {
         reader = new Reader();
         Set<Lending> lending = new HashSet<>();
         lending.add(new Lending());
         reader.setLendings(lending);
         when(req.getParameter("selected")).thenReturn("1");
-        when(service.getReader(1)).thenReturn(reader);
-        when(service.isReaderLendingSetEmpty(1)).thenReturn(false);
-        readerController.createSelectedReaderId(req);
-        readerController.returnAction(resp);
+        when(readerService.getReader(login, 1)).thenReturn(reader);
+        when(readerService.isReaderLendingSetEmpty(login, 1)).thenReturn(false);
+        readerController.returnAction(req, resp);
         assertAll(
                 () -> verify(session).setAttribute("lendings", lending),
                 () -> verify(session).setAttribute("reader", reader),
@@ -83,31 +100,25 @@ class ReaderControllerTest {
     }
 
     @Test
-    void test_resolveLend() {
-        readerController.setSession(session);
+    void test_lendAction() {
         reader = new Reader();
         when(req.getParameter("selected")).thenReturn("1");
-        when(service.getReader(1)).thenReturn(reader);
-        readerController.setReaderService(service);
-        readerController.createSelectedReaderId(req);
-        readerController.lendAction();
+        when(readerService.getReader(login, 1)).thenReturn(reader);
+        readerController.lendAction(req);
         verify(session).setAttribute("selReader", reader);
     }
 
     @Test
-    void test_resolveDelete_when_selectedReaders_lendingSet_isEmpty() throws IOException, ServletException {
-        List<Reader> readers = new ArrayList<>();
-        RequestDispatcher rd = mock(RequestDispatcher.class);
+    void test_deleteAction_when_selectedReaders_lendingSet_isEmpty() throws IOException, ServletException {
+        readers = new ArrayList<>();
+        readers.add(new Reader());
         when(req.getRequestDispatcher(captor.capture())).thenReturn(rd);
         when(req.getParameter("selected")).thenReturn("1");
-        when(service.deleteIfPossible(1)).thenReturn(true);
-        when(service.getReadersList(new Login())).thenReturn(readers);
-        readerController.setReaderService(service);
-        readerController.setSession(session);
-        readerController.createSelectedReaderId(req);
+        when(readerService.deleteIfPossible(login, 1)).thenReturn(true);
+        when(readerService.getReadersList(login)).thenReturn(readers);
         readerController.deleteAction(req, resp);
         assertAll(
-                () -> verify(service).deleteIfPossible(1),
+                () -> verify(readerService).deleteIfPossible(login, 1),
                 () -> verify(session).setAttribute("readers", readers),
                 () -> verify(req).getRequestDispatcher(captor.capture()),
                 () -> assertEquals("reader.jsp", captor.getValue())
@@ -116,23 +127,14 @@ class ReaderControllerTest {
 
 
     @Test
-    void test_resolveEdit(){
-        readerController.setSession(session);
+    void test_editAction() {
         when(req.getParameter("selected")).thenReturn("1");
-        readerController.setReaderService(service);
-        readerController.createSelectedReaderId(req);
-        readerController.editAction();
-        verify(session).setAttribute("readerToEdit", readerController.getSelectedReader());
+        reader = new Reader(1);
+        when(readerService.getReader(login, 1)).thenReturn(reader);
+        readerController.editAction(req);
+        verify(session).setAttribute("edit", reader);
     }
 
-    @Test
-    void test_createProperSelectedReader() {
-        when(req.getParameter("selected")).thenReturn("1");
-        readerController.setReaderService(service);
-        readerController.createSelectedReaderId(req);
-        readerController.getSelectedReader();
-        verify(service).getReader(1);
-    }
 
     @Test
     void test_doPost_with_wrong_button_value() {
@@ -144,8 +146,6 @@ class ReaderControllerTest {
     void test_doPost_when_button_name_equals_edit() throws ServletException, IOException {
         when(req.getParameter("button")).thenReturn("edit");
         when(req.getParameter("selected")).thenReturn("1");
-        readerController.setReaderService(service);
-        readerController.setSession(session);
         readerController.doPost(req, resp);
         assertAll(
                 () -> verify(resp).sendRedirect(captor.capture()),
@@ -156,8 +156,8 @@ class ReaderControllerTest {
     @Test
     void test_doPost_when_button_name_equals_lend() throws ServletException, IOException {
         when(req.getParameter("button")).thenReturn("lend");
-        readerController.setSession(session);
-        readerController.setReaderService(service);
+        when(req.getParameter("selected")).thenReturn("1");
+        when(readerService.getReader(login, 1)).thenReturn(new Reader());
         readerController.doPost(req, resp);
         assertAll(
                 () -> verify(resp).sendRedirect(captor.capture()),
