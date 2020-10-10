22da5e5153c82b345612cefee5cbf4693fff4bbe
BG678 <57709951+BG678@users.noreply.github.com>
2020-10-11 01:48:35 +0200

    Created tests for ReturnBookService class

--- library/src/test/java/org/library/features/return_book/ReturnBookServiceTest.java
+++ library/src/test/java/org/library/features/return_book/ReturnBookServiceTest.java
@@ -1,5 +1,79 @@
+package org.library.features.return_book;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.library.features.lend_book.Lending;
+import org.library.features.login.Login;
+import org.library.features.reader.Reader;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class ReturnBookServiceTest {
+    private ReturnBookService returnBookService;
+    private ReturnBookDAO returnBookDAO;
+    private Login login;
+    private List<Reader> readers;
+    @Mock
+    private ReturnBookDAOImpl returnBookDAOImpl;
 
+    @BeforeEach
+    void setUp() {
+        returnBookService = new ReturnBookService();
+        MockitoAnnotations.openMocks(this);
+        login = new Login();
+        returnBookDAO = returnBookDAOImpl;
+        returnBookService.setReturnBookDAO(returnBookDAO);
+    }
+
+    @AfterEach
+    void tearDown() {
+        returnBookService = null;
+        returnBookDAO = null;
+    }
+
+    @Test
+    void test_deleteLending() {
+        returnBookService.deleteLending(1);
+        verify(returnBookDAO).delete(any(Lending.class));
+    }
+
+    @Test
+    void test_getReader() {
+       createProperListOfReaders();
+        when(returnBookDAO.getReadersList(login)).thenReturn(readers);
+        assertEquals(returnBookService.getReader(login, 1), readers.get(0));
+    }
+
+    @Test
+    void test_getActiveReadersList() {
+        createProperListOfReaders();
+        when(returnBookDAO.getReadersList(login)).thenReturn(readers);
+        assertEquals(returnBookService.getActiveReadersList(login).size(), 2);
+    }
+
+    private void createProperListOfReaders() {
+        readers = new ArrayList<>();
+        Set<Lending> lendingSet = new HashSet<>();
+        lendingSet.add(new Lending(1));
+        Reader reader1 = new Reader(1);
+        Reader reader2 = new Reader(2);
+        Reader reader3 = new Reader();
+        reader1.setLendings(lendingSet);
+        reader2.setLendings(lendingSet);
+        reader3.setLendings(new HashSet<>());
+        readers.add(reader1);
+        readers.add(reader2);
+        readers.add(reader3);
+    }
 }
