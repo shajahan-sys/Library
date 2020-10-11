5b5b23922ec482329c922434afdf898ecaf195f8
BG678 <57709951+BG678@users.noreply.github.com>
2020-10-11 20:12:51 +0200

    Added javaDoc to AddEditDAO interface

--- library/src/main/java/org/library/features/add_edit_utils/AddEditDAO.java
+++ library/src/main/java/org/library/features/add_edit_utils/AddEditDAO.java
@@ -2,7 +2,15 @@ package org.library.features.add_edit_utils;
 
 import org.hibernate.SessionFactory;
 
+/**
+ * Interface that enables Service class to save T objects and set SessionFactory object.
+ *
+ * @param <T> represents a model class
+ * @author Barbara Grabowska
+ * @version %I%, %G%
+ */
 public interface AddEditDAO<T> {
     void setSessionFactory(SessionFactory sessionFactory);
+
     void save(T object);
 }
