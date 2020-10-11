7241979a3c345e138827c419015d4fa616480af5
BG678 <57709951+BG678@users.noreply.github.com>
2020-10-11 20:11:03 +0200

    Added javaDoc to AddEditService interface

--- library/src/main/java/org/library/features/add_edit_utils/AddEditService.java
+++ library/src/main/java/org/library/features/add_edit_utils/AddEditService.java
@@ -1,6 +1,21 @@
 package org.library.features.add_edit_utils;
 
+/**
+ * Service interface that enables Controller class
+ * to save T objects using AddEditDAO instance.
+ *
+ * @param <T> represents a model class
+ * @author Barbara Grabowska
+ * @version %I%, %G%
+ */
 public interface AddEditService<T> {
+    /**
+     * @param object to save
+     */
     void save(T object);
+
+    /**
+     * @param addEditDAO to set
+     */
     void setAddEditDAO(AddEditDAO<T> addEditDAO);
 }
