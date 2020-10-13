b0bf2c73db15625cdd7cfe3fdfc4096e284a580d
BG678 <57709951+BG678@users.noreply.github.com>
2020-10-13 17:15:40 +0200

    Added javadoc

---
+++ library/src/main/java/org/library/features/login/LoginDAO.java
@@ -0,0 +1,50 @@
+package org.library.features.login;
+
+import org.hibernate.SessionFactory;
+
+/**
+ * DAO interface for a login feature, contains methods that are
+ * necessary for user to sing in or sign up.
+ *
+ * @author Barbara Grabowska
+ * @version %I%, %G%
+ */
+public interface LoginDAO {
+    /**
+     * Inserts new account(Login object) into a database.
+     *
+     * @param login Login object that stores proper data
+     */
+    void saveNewAccount(Login login);
+
+    /**
+     * Checks if a login with given username already exists in a database.
+     *
+     * @param login Login object
+     * @return true if any instance matches the query, otherwise false
+     */
+    boolean doesUserAlreadyExist(Login login);
+
+    /**
+     * Fetches adequate id (according to username) form database
+     * and sets it as a passed Login object's id.
+     *
+     * @param login Login object
+     * @return Login object that contains proper data
+     */
+    Login getLogin(Login login);
+
+    /**
+     * @param factory SessionFactory object to set
+     */
+    void setSessionFactory(SessionFactory factory);
+
+    /**
+     * Fetches a hashed password from database that is linked to
+     * a given login's username and returns it.
+     *
+     * @param login Login object
+     * @return hashedPassword that belongs to a given user, or null
+     */
+    String getHashedPassword(Login login);
+}
