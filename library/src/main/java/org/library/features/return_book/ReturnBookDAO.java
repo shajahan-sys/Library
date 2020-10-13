2c427c73d41732384eada92982ff38047250f02d
BG678 <57709951+BG678@users.noreply.github.com>
2020-10-13 17:57:07 +0200

    Added javadoc comments

--- library/src/main/java/org/library/features/return_book/ReturnBookDAO.java
+++ library/src/main/java/org/library/features/return_book/ReturnBookDAO.java
@@ -7,8 +7,32 @@ import org.library.features.reader.Reader;
 
 import java.util.List;
 
+/**
+ * DAO interface for a return_book feature, contains methods that are used by a
+ * Service class to get a proper list of active Readers and to delete lending.
+ *
+ * @author Barbara Grabowska
+ * @version %I%, %G%
+ */
 public interface ReturnBookDAO {
+    /**
+     * @param sessionFactory to set
+     */
     void setSessionFactory(SessionFactory sessionFactory);
-    List<Reader> getReadersList(Login login);
+
+    /**
+     * Fetches list of Reader objects from database that have not empty
+     * Lending Set and belong to a given login.
+     *
+     * @param login Login object
+     * @return List of Readers who have at least 1 book on loan and are linked with provided login
+     */
+    List<Reader> getActiveReadersList(Login login);
+
+    /**
+     * Removes a given Lending object from a database.
+     *
+     * @param lending that is about to be deleted
+     */
     void delete(Lending lending);
 }
