9727bd01af1a6919c429d1138052eb67fd61e140
BG678 <57709951+BG678@users.noreply.github.com>
2020-10-13 17:37:50 +0200

    Added javadoc, optimized getBooksList, getAuthorsList and delete methods

--- library/src/main/java/org/library/features/book/BookDAOImpl.java
+++ library/src/main/java/org/library/features/book/BookDAOImpl.java
@@ -1,19 +1,28 @@
 package org.library.features.book;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.library.features.author.Author;
 import org.library.features.login.Login;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
+/**
+ * BookDAO implementation.
+ *
+ * @author Barbara Grabowska
+ * @version %I%, %G%
+ */
 public class BookDAOImpl implements BookDAO {
     private SessionFactory sessionFactory;
-    private Logger logger = LogManager.getLogger(BookDAOImpl.class);
+    /**
+     * Logger instance for this class
+     */
+    private final Logger logger = LoggerFactory.getLogger(BookDAOImpl.class);
 
     @Override
     public void setSessionFactory(SessionFactory sessionFactory) {
@@ -22,73 +31,38 @@ public class BookDAOImpl implements BookDAO {
 
     @Override
     public List<Book> getBooksList(Login login) {
-        List books;
-        Transaction transaction = null;
-
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.getTransaction();
-            transaction.begin();
-            books = session.createQuery("from Book where user_id = :id order by title")
-                    .setParameter("id", login.getId())
-                    .getResultList();
-            transaction.commit();
-        } catch (RuntimeException e) {
-            logger.error(e.getMessage());
-            try {
-                if (transaction != null)
-                    transaction.rollback();
-            } catch (HibernateException e1) {
-                logger.error("Transaction rollback not successful");
-            }
-            throw e;
-        }
+        Session session = sessionFactory.openSession();
+        List<Book> books = session.createQuery("from Book where user_id = :id order by title")
+                .setParameter("id", login.getId())
+                .list();
+        session.close();
         return books;
     }
 
     @Override
     public List<Author> getAuthorsList(Login login) {
-        List authors;
-        Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.getTransaction();
-            transaction.begin();
-            authors = session.createQuery("from Author where user_id = :id order by surname")
-                    .setParameter("id", login.getId())
-                    .getResultList();
-            transaction.commit();
-        } catch (RuntimeException e) {
-            logger.error(e.getMessage());
-            try {
-                if (transaction != null)
-                    transaction.rollback();
-            } catch (HibernateException e1) {
-                logger.error("Transaction rollback not successful");
-            }
-            throw e;
-        }
+        Session session = sessionFactory.openSession();
+        List<Author> authors = session.createQuery("from Author where user_id = :id order by surname")
+                .setParameter("id", login.getId())
+                .list();
+        session.close();
         return authors;
     }
 
     @Override
     public void delete(Book book) {
-        Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.getTransaction();
-            transaction.begin();
-            book = session.get(Book.class, book.getId());
-            if (book != null) {
-                session.delete(book);
-            }
-            transaction.commit();
-        } catch (RuntimeException e) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.delete(book);
+            tx.commit();
+        } catch (HibernateException e) {
             logger.error(e.getMessage());
-            try {
-                if (transaction != null)
-                    transaction.rollback();
-            } catch (HibernateException e1) {
-                logger.error("Transaction rollback not successful");
-            }
-            throw e;
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
         }
     }
 }
