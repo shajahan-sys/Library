70872de178e3137900b4cb4f109dfb30f0a58f58
BG678 <57709951+BG678@users.noreply.github.com>
2020-10-13 17:58:19 +0200

    Added javadoc comments, improved delete and getActiveReadersList methods

--- library/src/main/java/org/library/features/return_book/ReturnBookDAOImpl.java
+++ library/src/main/java/org/library/features/return_book/ReturnBookDAOImpl.java
@@ -1,19 +1,34 @@
 package org.library.features.return_book;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.library.features.lend_book.Lending;
 import org.library.features.login.Login;
+import org.library.features.reader.Reader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
+/**
+ * ReturnBookDAO implementation.
+ *
+ * @author Barbara Grabowska
+ * @version %I%, %G%
+ */
 public class ReturnBookDAOImpl implements ReturnBookDAO {
     private SessionFactory sessionFactory;
-    private final Logger logger = LogManager.getLogger(ReturnBookDAOImpl.class);
+    /**
+     * Logger instance for this class
+     */
+    private final Logger logger = LoggerFactory.getLogger(ReturnBookDAOImpl.class);
 
 
     @Override
@@ -22,49 +37,33 @@ public class ReturnBookDAOImpl implements ReturnBookDAO {
     }
 
     @Override
-    public List getReadersList(Login login) {
-        List readers;
-        Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.getTransaction();
-            transaction.begin();
-            readers = session.createQuery("from Reader where user_id = :id order by surname")
-                 //   .setParameter("id", login.getId())
-                    .setParameter("id", login.getId())
-                    .getResultList();
-            transaction.commit();
-        } catch (RuntimeException e) {
-            logger.error(e.getMessage());
-            try {
-                if (transaction != null)
-                    transaction.rollback();
-            } catch (HibernateException e1) {
-                logger.error("Transaction rollback not successful");
-            }
-            throw e;
-        }
-        return readers;
+    public List<Reader> getActiveReadersList(Login login) {
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Reader> cr = cb.createQuery(Reader.class);
+        Root<Reader> root = cr.from(Reader.class);
+        Predicate[] predicates = new Predicate[2];
+        predicates[0] = cb.equal(root.get("login"), login.getId());
+        predicates[1] = cb.isNotEmpty(root.get("lendings"));
+        cr.select(root).where(predicates);
+        Query<Reader> query = session.createQuery(cr);
+        return query.getResultList();
     }
 
     @Override
     public void delete(Lending lending) {
-        Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            lending = session.get(Lending.class, lending.getId());
-            if (lending != null) {
-                session.delete(lending);
-            }
-            transaction.commit();
-        } catch (RuntimeException e) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.delete(lending);
+            tx.commit();
+        } catch (HibernateException e) {
             logger.error(e.getMessage());
-            try {
-                if (transaction != null)
-                    transaction.rollback();
-            } catch (HibernateException e1) {
-                  logger.error("Transaction rollback not successful");
-            }
-            throw e;
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
         }
     }
 }
